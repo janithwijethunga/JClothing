217d93123bed80fec7dee57c97fb8a912cfeb9a5
janithwijethunga <janithwijethunga68@gmail.com>
2024-07-11 14:20:12 +0530

    create app and Navbar

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import logo from '../Assets/logo.png'
+
+function Navbar() {
+  return (
+    <div>
+        <div>
+            <img src={logo} alt="" className=''/>
+            <p>JClothing</p>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
